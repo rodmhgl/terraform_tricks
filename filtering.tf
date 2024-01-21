8e62373b56a9d981fa68e8fe4e8f14c0d0ddd276
Rod Stewart <rodstewart@gmail.com>
2024-01-21 10:59:17 -0600

    Adds filtering example

---
+++ filtering.tf
@@ -0,0 +1,35 @@
+# Similar to the conditional deployments example,
+# if conditions can be used to filter a list of objects to create new objects.
+
+variable "users" {
+  type = map(object({
+    is_admin = bool
+  }))
+  default = {
+    "bob_user" = {
+      is_admin = false
+    }
+    "tom_admin" = {
+      is_admin = true
+    }
+  }
+}
+
+locals {
+  admin_users = {
+    for name, user in var.users : name => user
+    if user.is_admin
+  }
+  regular_users = {
+    for name, user in var.users : name => user
+    if !user.is_admin
+  }
+}
+
+output "filtering_admin_users" {
+  value = { for k, v in local.admin_users : k => v }
+}
+
+output "filtering_regular_users" {
+  value = { for k, v in local.regular_users : k => v }
+}
