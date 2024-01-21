0a20091b4f40ab0b94e83e0a614937ad553d033f
Rod Stewart <rodstewart@gmail.com>
2024-01-21 11:00:59 -0600

    Adds main

--- main.tf
+++ main.tf
@@ -1,29 +1,10 @@
-locals {
-  deploy_if_map_value_is_true = {
-    deploy_true = {
-      name = "Deploy Me"
-      enabled = true
-    }
-    deploy_false = {
-      name = "Do Not Deploy Me"
-      enabled = false
-    }
-  }
-
+provider "azurerm" {
+  features {}
 }
 
-resource "null_resource" "deploy_if_map_value_is_true" {
-
-  for_each = { for k, v in local.deploy_if_map_value_is_true : k => v if v.enabled == true }
-
-  triggers = {
-    always_run = "${timestamp()}"
+locals {
+  tags = {
+    environment = "dev"
+    purpose     = "terraform_tips_and_tricks"
   }
-
-  provisioner "local-exec" {
-    command = <<-EOT
-      echo -e "\n***Hello, ${each.value.name}!***\n"
-    EOT
-  }
-
 }
