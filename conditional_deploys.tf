8eacd2d751c0214bed688bf013a940efe33221d3
Rod Stewart <rodstewart@gmail.com>
2024-01-21 19:08:40 -0600

    Adds variable

--- conditional_deploys.tf
+++ conditional_deploys.tf
@@ -1,5 +1,10 @@
-locals {
-  resource_groups = {
+variable "resource_groups" {
+  type = map(object({
+    enabled  = bool
+    location = string
+  }))
+  description = "A map of resource groups to create."
+  default = {
     rg-app-dev-use = {
       enabled  = true
       location = "East US"
@@ -12,16 +17,19 @@ locals {
 }
 
 resource "azurerm_resource_group" "conditional_deploys" {
+  # Dynamically generate a map of enabled resource groups
+  # This logic could also be moved to a locals block if preferred
+  # for_each = local.enabled_resource_groups
   for_each = {
-    for k, v in local.resource_groups :
+    for k, v in var.resource_groups :
     k => v if v.enabled == true
   }
 
-  location = each.value.location
   name     = each.key
+  location = each.value.location
   tags     = local.tags
 }
 
-output "conditional_deploys_resource_groups" {
-  value = { for k, v in azurerm_resource_group.conditional_deploys : k => v }
+output "conditional_deploys_resource_group_names" {
+  value = [for k, v in azurerm_resource_group.conditional_deploys : k]
 }
