3c772266bda0ebc8697bb7d128f3efdbca736df7
Rod Stewart <rodstewart@gmail.com>
2024-01-21 19:08:57 -0600

    fmt, neater outputs

--- filtering.tf
+++ filtering.tf
@@ -1,6 +1,5 @@
 # Similar to the conditional deployments example,
 # if conditions can be used to filter a list of objects to create new objects.
-
 variable "users" {
   type = map(object({
     is_admin = bool
@@ -28,11 +27,11 @@ locals {
 }
 
 output "filtering_admin_users" {
-  description = "A map of admin users."
-  value       = { for k, v in local.admin_users : k => v }
+  description = "A list of admin users."
+  value       = [for k, v in local.admin_users : k]
 }
 
 output "filtering_regular_users" {
-  description = "A map of regular users."
-  value       = { for k, v in local.regular_users : k => v }
+  description = "A list of regular users."
+  value       = [for k, v in local.regular_users : k]
 }
